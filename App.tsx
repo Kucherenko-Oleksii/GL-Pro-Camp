c796b79a4e8c6e4b4296c4baced344b9168bacb3
Oleksii Kucherenko <kucherenkoolexiy@gmail.com>
2021-10-22 12:42:41 +0300

    Myapp_files

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import  React from 'react';
+import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
+import MainStack from './navigate';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text></Text>
-    </View>
+      <MainStack /> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles =  StyleSheet.create({
+   mainstyle:
+   {
+     backgroundColor: 'black'
+   }
+}) 
